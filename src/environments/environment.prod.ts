96393672efb8f27dedeaf24e68d755340bb6bf66
jhonatan.ospina <jhonatan.ospina@utp.edu.co>
2023-03-03 00:58:05 -0500

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.221.137.237/banking_api/v1/'
+  apiUrl: 'https://jhon1409.pythonanywhere.com/banking_api/v1/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://18.221.137.237/banking_api/v1/'
+  apiUrl: 'https://jhon1409.pythonanywhere.com/banking_api/v1/'
 };
 
 /*
